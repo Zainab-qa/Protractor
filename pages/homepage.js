96ef57c8cb1e6a0f0f73f4aafdda8dbf6810f2b9
zainab <zainab.malhi@outlook.com>
2020-08-31 22:47:54 -0400

    Protractor project

---
+++ pages/homepage.js
@@ -0,0 +1,34 @@
+const { Browser } = require("selenium-webdriver");
+
+let homepage = function(){
+
+    //locators
+    let firstNumber_input = element(by.model('first'))
+    let secondNumber_input = element(by.model('second'))
+    let gotButton = element(by.css('[ng-click="doAddition()"]'))
+    let output = element(by.cssContainingText('.ng-binding','7'))
+   
+    this.get = function(url){
+        browser.get(url)
+    }
+
+    this.enterFirstNumber = function(firstNo){
+        firstNumber_input.sendKeys(firstNo);
+    }
+    this.enterSecondNumber = function(secondNo){
+        secondNumber_input.sendKeys(secondNo)
+    }
+    this.clickGo = function(){
+        gotButton.click();
+    }
+    this.verifyResult = function(result){
+        expect(output.getText()).toEqual(result);
+    }
+   
+
+
+
+
+};
+
+module.exports = new homepage();
